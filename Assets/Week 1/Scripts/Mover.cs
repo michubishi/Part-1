c9250bbf344b9e0a641e56befa7b15d8b29d6214
vuomiche <vuomiche@sheridancollege.ca>
2024-01-11 15:21:41 -0500

    Task 2b complete spawned missiles

--- Assets/Week 1/Scripts/Mover.cs
+++ Assets/Week 1/Scripts/Mover.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     float speed = 5f;
+    public GameObject missilePrefab;
+    public Transform spawn;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +18,11 @@ public class Mover : MonoBehaviour
     {
         float direction = Input.GetAxis("Horizontal");
         transform.Translate(direction * speed * Time.deltaTime, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //make missile appear
+            Instantiate(missilePrefab, spawn.position, spawn.rotation);
+        }
     }
 }
