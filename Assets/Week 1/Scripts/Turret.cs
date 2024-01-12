9292f1e93fa0477565d70fb29ad07dfdfe0b1298
Michelle <vuong.michelle2005@gmail.com>
2024-01-11 21:10:46 -0500

    Task 4b completed. Made barrel move on input. (Typo in previous commit message).

--- Assets/Week 1/Scripts/Turret.cs
+++ Assets/Week 1/Scripts/Turret.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class Turret : MonoBehaviour
 {
     float speed = 30f;
-    // Start is called before the first frame update
+  
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
         float direction = Input.GetAxis("Vertical");
