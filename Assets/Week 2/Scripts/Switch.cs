0ac303b1e57fb16e1968fec1890f347e66bb55fc
vuomiche <vuomiche@sheridancollege.ca>
2024-01-18 15:06:52 -0500

    Task 2 complete

---
+++ Assets/Week 2/Scripts/Switch.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Android.Types;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    public Color myColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = myColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger from " +  collision.gameObject);
+        spriteRenderer.color = Color.red;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        spriteRenderer.color = myColor;
+    }
+}
