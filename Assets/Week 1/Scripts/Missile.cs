fa856aaf8dea655cb5053559e7d648de057a4a54
vuomiche <vuomiche@sheridancollege.ca>
2024-01-18 14:20:06 -0500

    Task 1 complete missile collisons

--- Assets/Week 1/Scripts/Missile.cs
+++ Assets/Week 1/Scripts/Missile.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class Missile : MonoBehaviour
 {
-    float speed = 5f;
-
+    public float speed = 5f;
+    Rigidbody2D rigidbody;
+    Vector2 direction = new Vector2(10, 0);
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(1 * speed * Time.deltaTime, 0, 0); 
+        //transform.Translate(1 * speed * Time.deltaTime, 0, 0); 
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + direction * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Missile hit something");
+        Destroy(gameObject);
     }
 }
