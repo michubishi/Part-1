797bb31c2b34df9e0a8d6c346f677b488807c366
Michelle <vuong.michelle2005@gmail.com>
2024-01-18 21:43:35 -0500

    Task 4b complete. Added door and lever. Player can interact with lever. Missile can't collide with lever.

---
+++ Assets/Week 2/Scripts/Lever.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    public GameObject door;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("haha");
+        if (door.activeInHierarchy)
+        {
+            door.SetActive(false);
+        }
+        else
+        {
+            door.SetActive(true);
+        }
+    }
+}
